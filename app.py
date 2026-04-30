b2cf2d72b32f840643ba18dedc31fede31a88786
C2A503-17 <C2A503-17@sdom.labsing>
2026-04-29 20:14:32 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Resultados Electorales ONPE 2021")
+
+st.write("Sistema de análisis, visualización y despliegue de resultados por mesa de la Segunda Elección Presidencial 2021.")
